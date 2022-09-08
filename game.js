3ae1360c66d8d8942aa8915439b6fe8928824fd6
Shubham-407 <108684617+Shubham-407@users.noreply.github.com>
2022-09-08 20:23:59 +0530

    Create game.js

---
+++ game.js
@@ -0,0 +1,63 @@
+  const holes = document.querySelectorAll('.hole');
+  const scoreBoard = document.querySelector('#score');
+  const moles = document.querySelectorAll('.square');
+  let lastHole;
+  let timeUp = false;
+  let score = 0;
+
+  function randomTime(min, max) { 
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+      return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+  }
+
+  function peep() {
+    const time = randomTime(1000, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+      hole.classList.remove('up');
+      if (!timeUp) peep();
+    }, time);
+  }
+
+  function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000)
+  }
+  function poop() {
+    const time = randomTime(0, 0);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+      hole.classList.remove('up');
+      if (!timeUp) peep();
+    }, time);
+  }
+  function resetGame(){
+    scoreBoard.textContent = 0;
+    timeUp = true;
+    score = 0;
+    poop();
+    setTimeout(() => timeup = true, 0)
+  }
+
+  function whack(e) {
+    if(!e.isTrusted) return; 
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+  }
+
+  moles.forEach(mole => mole.addEventListener('click', whack));
